2b7554bc77fa09fcc9ce09a2dcf4737d28dc333f
Ernesto Resende <dev@ernestoresende.com>
2022-12-30 02:35:37 -0300

    chore: add src files

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateGoogleCloudFunctionsHandler } from './gcp';
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+startServerAndCreateGoogleCloudFunctionsHandler(server)
+
